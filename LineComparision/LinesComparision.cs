a168b609222393b58a37516cf4fcb9a729661cb6
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 22:10:10 +0530

    Comparing Two Lines both are equal or greater or less

--- LineComparision/LinesComparision.cs
+++ LineComparision/LinesComparision.cs
@@ -10,8 +10,8 @@ namespace LineComparision
     {
         public static void LinesLengthsComparision()
         {
-            int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
-            double x = 0, y = 0, length = 0;
+            int x1 = 0, x2 = 0, y1 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0;
+            double x = 0, y = 0, length = 0, X = 0, Y = 0, length1 = 0;
             Console.WriteLine("Enter X1 and Y1 Cordinate Values");
             x1 = Convert.ToInt32(Console.ReadLine());
             y1 = Convert.ToInt32(Console.ReadLine());
@@ -22,7 +22,27 @@ namespace LineComparision
             x = Math.Pow((x2 - x1), 2);
             y = Math.Pow((y2 - y1), 2);
             length = Math.Sqrt(x + y);
-            Console.WriteLine("Length of line: " + length);
+            Console.WriteLine("Length of line1: " + length);
+
+            Console.WriteLine("\nEnter X3 and Y3 Cordinate Values");
+            x3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X4 and Y4 Cordinate Values");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+            x = Math.Pow((x4 - x3), 2);
+            y = Math.Pow((y4 - y3), 2);
+            length1 = Math.Sqrt(X + Y);
+            Console.WriteLine("Length of line2: " + length1);
+            if (length > length1)
+            {
+                Console.WriteLine("Line1 is grater: " + length);
+            }
+            else
+            {
+                Console.WriteLine("Line2 is greater: " + length1);
+            }
         }
     }
 }
